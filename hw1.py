4bc14f03c80e9cfc6a90f0468e7f37500cf63277
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:20:21 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,9 @@
+﻿str1 = """\t"Stay hungry. Stay foolish"
+And I have always wished that for myself.
+And now, as you graduate to begin anew, I wish that for you."""
+
+str2 = "\t\"Stay hungry. Stay foolish\"\nAnd I have always wished that for myself.\nAnd now, as you graduate to begin anew, I wish that for you."
+
+print(str1)
+print()
+print(str2)
---
+++ hw2.py
@@ -0,0 +1,15 @@
+﻿color = input('좋아하는 색깔은 :')
+
+print(color*len(color))
+print()
+
+num1 = int(input('정수1 을 입력 : '))
+num2 = int(input('정수2 을 입력 : '))
+
+print(num1, '와', num2, '의 합 :', num1+num2)
+print(num1, '에서', num2, '의 차 :', num1-num2)
+print(num1, '와', num2, '의 곱 :', num1*num2)
+print(num1, '에서', num2, '를 나눈값 :', num1/num2)
+print(num1, '에서', num2, '를 나눈 몫 :', num1//num2)
+print(num1, '에서', num2, '를 나눈 나머지 :', num1%num2)
+print(num1, '의', num2, '승 :', num1**num2)
